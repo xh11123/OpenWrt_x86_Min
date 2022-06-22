a4967156307993101780d749d1116d42a5b7e6a0
xh11123 <2683329762@qq.com>
2022-06-22 10:03:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git vmtools https://github.com/fangli/openwrt-vm-tools' >>feeds.conf.default
+#echo 'src-git vmtools https://github.com/fangli/openwrt-vm-tools' >>feeds.conf.default
